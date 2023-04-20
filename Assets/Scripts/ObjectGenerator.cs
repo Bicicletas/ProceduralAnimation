c7dd58aa216889f413beaaaaac18cd35a250f52d
Bicicletas <piter.aa22@gmail.com>
2023-04-20 15:53:15 +0200

    caca

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -6,46 +6,13 @@ public class ObjectGenerator : MonoBehaviour
 {
     [SerializeField] float spawnDelay = 0.8f;
     [SerializeField] float sizeOffset = 50;
-    [SerializeField] float rayOriginY = 80;
-
-
-    [System.Serializable]
-    public class Settings
-    {
-        public string objectName;
-        public int numOfObject;
-        public LayerMask whatIsGround;
-        public float minObjectSpawnThreshold;
-        public float maxObjectSpawnThreshold;
-        public float objectSpawnOffsetY = 0.1f;
-        public float minScale = 1;
-        public float maxScale = 2;
-        public GameObject[] objects;
-        [HideInInspector] public List<Vector3> positions = new List<Vector3>();
-
-        public float RandomRotation()
-        {
-            float angle = Random.Range(0, 360);
-
-            return angle;
-        }
-
-        public float RandomScale()
-        {
-            float index = Random.Range(minScale, maxScale);
-
-            return index;
-        }
-
-        public int RandomIndex()
-        {
-            int index = Random.Range(0, objects.Length);
-
-            return index;
-        }
-    }
+    [SerializeField] float rayOriginY = 80;    
 
     [SerializeField] Settings[] settings;
+
+    [HideInInspector] public List<Vector3> positions = new List<Vector3>();
+
+    int objAmount = 0;
 
     private void Start()
     {
@@ -66,28 +33,45 @@ public class ObjectGenerator : MonoBehaviour
         {
             for (int i = 0; i < s.numOfObject; i++)
             {
-                s.positions.Add(new Vector3(RandomPos(transform.parent.position).x, rayOriginY, RandomPos(transform.parent.position).y));
+                positions.Add(new Vector3(RandomPos(transform.parent.position).x, rayOriginY, RandomPos(transform.parent.position).y));
 
                 RaycastHit hit;
 
-                if (Physics.Raycast(s.positions[i], Vector3.down, out hit, 100f, s.whatIsGround))
+                if (Physics.Raycast(positions[i], Vector3.down, out hit, 100f, s.whatIsGround))
                 {
                     if (hit.point.y > s.minObjectSpawnThreshold && hit.point.y < s.maxObjectSpawnThreshold)
                     {
-                        GameObject @object = s.objects[s.RandomIndex()];
+                        GameObject instance = null;
 
-                        GameObject instance = Instantiate(@object, new Vector3(hit.point.x, hit.point.y - s.objectSpawnOffsetY, hit.point.z), Quaternion.Euler(@object.transform.eulerAngles.x, @object.transform.eulerAngles.y + s.RandomRotation(), @object.transform.eulerAngles.z));
+                        if (s.randomSpawn.randomScale == 0)
+                        {
+                            InstantiateObject(s, instance, hit);
+                        }
+                        else if (s.randomSpawn.RandomizedSpawnRate())
+                        {
+                            InstantiateObject(s, instance, hit);
 
-                        instance.transform.parent = hit.transform;
-
-                        float scale = s.RandomScale();
-
-                        instance.transform.localScale = new Vector3(scale, scale, scale);
+                            objAmount++;
+                        }
                     }
                 }
             }
         }
+
+        print(objAmount);
     }
 
-    
+    void InstantiateObject(Settings s, GameObject instance, RaycastHit hit)
+    {
+        GameObject @object = s.objects[s.RandomIndex()];
+
+        instance = Instantiate(@object, new Vector3(hit.point.x, hit.point.y + s.objectSpawnOffsetY, hit.point.z),
+            Quaternion.Euler(@object.transform.eulerAngles.x, @object.transform.eulerAngles.y + s.RandomRotation(), @object.transform.eulerAngles.z));
+
+        instance.transform.parent = hit.transform;
+
+        float scale = s.RandomScale();
+
+        instance.transform.localScale = new Vector3(scale, scale, scale);
+    }
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class Settings : ScriptableObject
+{
+    [Min(0)]
+    public int numOfObject = 1;
+    public LayerMask whatIsGround;
+    [Range(0, 80)]
+    public float minObjectSpawnThreshold = 10;
+    [Range(0, 80)]
+    public float maxObjectSpawnThreshold = 30;
+    public float objectSpawnOffsetY = 0.1f;
+    [Min(1)]
+    public float minScale = 1;
+    [Min(1)]
+    public float maxScale = 2;
+    public GameObject[] objects;
+
+    [System.Serializable]
+    public class RandomSpawn
+    {
+        [Tooltip("Applied if random scale is greater than 0")]
+        public int randomScale = 10;
+
+        public bool RandomizedSpawnRate()
+        {
+            int index = Random.Range(0, randomScale);
+
+            if(index == (randomScale - 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public RandomSpawn randomSpawn;
+
+    public float RandomRotation()
+    {
+        float angle = Random.Range(0, 360);
+
+        return angle;
+    }
+
+    public float RandomScale()
+    {
+        float index = Random.Range(minScale, maxScale);
+
+        return index;
+    }
+
+    public int RandomIndex()
+    {
+        int index = Random.Range(0, objects.Length);
+
+        return index;
+    }
+}
